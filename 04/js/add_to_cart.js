8d51244cdac06191d8de2c59a0333e644ba45fb4
yeji seo <yejiseo0725@gmail.com>
2022-07-11 12:34:36 +0900

    이미지 경로 설정

--- 04/js/add_to_cart.js
+++ 04/js/add_to_cart.js
@@ -46,97 +46,128 @@ $(function () {
     }
 
     amountPrice_func();
-    calc();
   });
 
   amountPrice_func();
-  calc();
 
   function amountPrice_func() {
     const $list1 = $(".list1");
-    const getPrice = $list1.find(".price").text(); // 금액 명시된 부분 찾기 (현재 string 인 상태)
-    const itemPrice = parseInt(getPrice.replace(/[^0-9]/g, "")); // 그 금액을 int 로 변환
-    const value = parseInt($list1.find("input[type=text]").val()); // 수량 부분도 찾아서 int 로 변환
+    const $price1 = $list1.find(".price").text().replace(/[^0-9]/g, "");
+    const $value1 = $list1.find("input[type=text]").val();
+    const $am1 = parseInt($price1) * parseInt($value1);
+    $list1.find(".am-price").text( $am1.toLocaleString() ); // 1번 장바구니의 합계
 
-    $list1.find(".am-price").text(itemPrice * value);
-    // 합계 부분 찾아서 int 로 변환한 금액 x 수량
-
-    // console.log(value);
 
     const $list2 = $(".list2");
-    const getPrice2 = $list2.find(".price").text();
-    const itemPrice2 = parseInt(getPrice2.replace(/[^0-9]/g, ""));
-    const value2 = parseInt($list2.find("input[type=text]").val());
+    const $price2 = $list2.find(".price").text().replace(/[^0-9]/g, ""); 
+    const $value2 = $list2.find("input[type=text]").val(); 
+    const $am2 = parseInt($price2) * parseInt($value2);
+    $list2.find(".am-price").text( $am2.toLocaleString() ); // 2번 장바구니의 합계 
 
-    $list2.find(".am-price").text(itemPrice2 * value2);
-
-    // console.log(itemPrice2, value2);
 
     const $list3 = $(".list3");
-    const getPrice3 = $list3.find(".price").text();
-    const itemPrice3 = parseInt(getPrice3.replace(/[^0-9]/g, ""));
-    const value3 = parseInt($list3.find("input[type=text]").val());
+    const $price3 = $list3.find(".price").text().replace(/[^0-9]/g, "");
+    const $value3 = $list3.find("input[type=text]").val(); 
+    const $am3 = parseInt($price3) * parseInt($value3);
+    $list3.find(".am-price").text( $am3.toLocaleString() );
 
-    // console.log(getPrice3, itemPrice3, value3);
+    console.log($am1 + $am2 + $am3);
 
-    $list3.find(".am-price").text(itemPrice3 * value3);
+    totalPay($am1, $am2, $am3);
+  }
+
+  function totalPay(a, b, c) {
+    const $am_sum = a + b + c;
+
+    const $total = $(".totalPrice-items");
+    const $sale = $total.find(".salePrice").text().replace(/[^0-9]/g, "");
+    const $delivery = $total.find(".deliveryPrice").text().replace(/[^0-9]/g, "");
+    const $calc = $am_sum - parseInt($sale) + parseInt($delivery);
+
+    $total.find(".amPrice-sum").text( $am_sum.toLocaleString() );
+    $total.find(".totalPrice").text( $calc.toLocaleString() );
   }
   // -----------
 
-  function calc() {
-    $where = $(".cartList");
-    list1_find = $where.find(".list1");
-    priceFind1 = parseInt(
-      list1_find
-        .find(".am-price")
-        .text()
-        .replace(/[^0-9]/g, "")
-    ); // 상품 금액 1
+  // function calc() {
+  //   $where = $(".cartList");
+  //   list1_find = $where.find(".list1");
+  //   priceFind1 = parseInt(
+  //     list1_find
+  //       .find(".am-price")
+  //       .text()
+  //       .replace(/[^0-9]/g, "")
+  //   ); // 상품 금액 1
 
-    list2_find = $where.find(".list2");
-    priceFind2 = parseInt(
-      list2_find
-        .find(".am-price")
-        .text()
-        .replace(/[^0-9]/g, "")
-    ); // 상품 금액 2
+  //   list2_find = $where.find(".list2");
+  //   priceFind2 = parseInt(
+  //     list2_find
+  //       .find(".am-price")
+  //       .text()
+  //       .replace(/[^0-9]/g, "")
+  //   ); // 상품 금액 2
 
-    list3_find = $where.find(".list3");
-    priceFind3 = parseInt(
-      list3_find
-        .find(".am-price")
-        .text()
-        .replace(/[^0-9]/g, "")
-    ); // 상품 금액 3
+  //   list3_find = $where.find(".list3");
+  //   priceFind3 = parseInt(
+  //     list3_find
+  //       .find(".am-price")
+  //       .text()
+  //       .replace(/[^0-9]/g, "")
+  //   ); // 상품 금액 3
 
-    $here = $(".cartTotal-box .totalPrice-items");
-    plusCalc = priceFind1 + priceFind2 + priceFind3;
-    $here.find(".amPrice-sum").text(plusCalc);
+  //   $here = $(".cartTotal-box .totalPrice-items");
+  //   plusCalc = priceFind1 + priceFind2 + priceFind3;
+  //   $here.find(".amPrice-sum").text(plusCalc);
 
-    console.log(priceFind1, priceFind2, priceFind3);
+  //   console.log(priceFind1, priceFind2, priceFind3);
 
-    // 할인 금액 & 배송비 제외
-    $whereM = $(".cartTotal-box .totalPrice-items");
+  //   // 할인 금액 & 배송비 제외
+  //   $whereM = $(".cartTotal-box .totalPrice-items");
 
-    discount = parseInt(
-      $whereM
-        .find(".salePrice")
-        .text()
-        .replace(/[^0-9]/g, "")
-    );
-    delivery = parseInt(
-      $whereM
-        .find(".deliveryPrice")
-        .text()
-        .replace(/[^0-9]/g, "")
-    );
+  //   discount = parseInt(
+  //     $whereM
+  //       .find(".salePrice")
+  //       .text()
+  //       .replace(/[^0-9]/g, "")
+  //   );
+  //   delivery = parseInt(
+  //     $whereM
+  //       .find(".deliveryPrice")
+  //       .text()
+  //       .replace(/[^0-9]/g, "")
+  //   );
 
-    // console.log(discount, delivery);
+  //   // console.log(discount, delivery);
 
-    // 총 결제 금액
-    totalPaymentPrice = plusCalc - discount + delivery;
-    // console.log(totalPaymentPrice);
+  //   // 총 결제 금액
+  //   totalPaymentPrice = plusCalc - discount + delivery;
+  //   // console.log(totalPaymentPrice);
 
-    $whereM.find(".totalPrice").text(totalPaymentPrice);
-  }
+  //   $whereM.find(".totalPrice").text(totalPaymentPrice);
+  // }
+
+  // 잠깐
+  // function totalPay() {
+  //   const list1_amPrice = parseInt($(".list1 .am-price").text());
+  //   const list2_amPrice = parseInt($(".list2 .am-price").text());
+  //   const list3_amPrice = parseInt($(".list3 .am-price").text());
+
+  //   const $total = $(".totalPrice-items");
+  //   const $sum = list1_amPrice + list2_amPrice + list3_amPrice;
+  //   const $sale = parseInt(
+  //     $total
+  //       .find(".salePrice")
+  //       .text()
+  //       .replace(/[^0-9]/g, "")
+  //   );
+  //   const $delivery = parseInt(
+  //     $total
+  //       .find(".deliveryPrice")
+  //       .text()
+  //       .replace(/[^0-9]/g, "")
+  //   );
+
+  //   $total.find(".amPrice-sum").text($sum);
+  //   $total.find(".totalPrice").text($sum - $sale + $delivery);
+  // }
 });
