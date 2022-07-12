4b2b93534a4a45d72c8da9f2ef0dfb5ebdcff64a
yeji seo <yejiseo0725@gmail.com>
2022-07-12 16:44:57 +0900

    [수정]  hasClass

--- 06/js/practice.js
+++ 06/js/practice.js
@@ -17,75 +17,47 @@ $(function () {
 
     $("#img0").trigger("click");
 
-    $(".arrow-down").click(function () {
-        if (img_index < 5) {
-            img_index++;
-        }
 
-        checkIndex();
+    $(".arrows button").click(function () {
+        if ( $(this).hasClass("arrow-up") ) {
+            if (img_index > 0) img_index--;
+        } else {
+            if (img_index < 5) img_index++;
+        }
 
         let $src = $(".img").eq(img_index).children().attr("src");
         $(".mainImage").children().attr("src", $src);
 
         $(".img").css("border", "none");
         $("#img" + img_index).css("border", "5px solid coral");
-        // console.log(img_index);
-
-    });
-
-    $(".arrow-up").click(function () {
-
-        if (img_index > 0) {
-            img_index--;
-
-            console.log(img_index);
-        }
 
         checkIndex();
-
-        let $src = $(".img").eq(img_index).children().attr("src");
-        $(".mainImage").children().attr("src", $src);
-
-        $(".img").css("border", "none");
-        $("#img" + img_index).css("border", "5px solid coral");
-        // console.log(img_index);
     });
 
+    
 
     function checkIndex() {
 
         if (img_index <= 0) {
             $(".arrow-up").prop("disabled", true);
             $(".arrow-up").css("cursor", "not-allowed");
+            // $(".arrow-down").children().css("color", "#808080");
         } else {
             $(".arrow-up").prop("disabled", false);
             $(".arrow-up").css("cursor", "pointer");
+            // $(".arrow-down").children().css("color", "black");
         }
 
         if (img_index >= 5) {
             $(".arrow-down").prop("disabled", true);
             $(".arrow-down").css("cursor", "not-allowed");
+            // $(".arrow-up").children().css("color", "#808080");
         } else {
             $(".arrow-down").prop("disabled", false);
             $(".arrow-down").css("cursor", "pointer");
+            // $(".arrow-up").children().css("color", "black");
         }
-
     };
-
-
-
-
-    // if (img_index >= 5) {
-    //     $(".arrow-down").children().css("color", "#808080");
-    // } else {
-    //     $(".arrow-down").children().css("color", "black");
-    // }
-    // if (img_index <= 0) {
-    //     $(".arrow-up").children().css("color", "#808080");
-    // } else {
-    //     $(".arrow-up").children().css("color", "black");
-    // }
-
 
 
 });
