c70816d19ffce8fcf4e4e933338de5b36b7cc6c9
yeji seo <yejiseo0725@gmail.com>
2022-07-15 16:11:02 +0900

    이렇게 어려워도 되는걸까,,

--- 08/js/version3.js
+++ 08/js/version3.js
@@ -8,14 +8,14 @@ $(function () {
 
     slider.on("init", function (event, slick) {
         sliderCounter(slick);
-        slideCheck(slick);
+        // slideCheck(slick);
         
         $(".progress").css({ width: calc * 100 + "%"});
         $(".pagingNum").text((slick.currentSlide + 1) + " / " + slick.slideCount);
 
     }).on("afterChange", function (event, slick, currentSlide, nextSlide) {
         sliderCounter(slick);
-        slideCheck(slick);
+        // slideCheck(slick);
 
         $(".pagingNum").text((nextSlide + 1) + " / " + slick.slideCount);
         $(".progress").css({ left: (slidesPer - calc) * 100 + "%" });
@@ -29,29 +29,29 @@ $(function () {
         calc = 1 / slidesCount;
     }
 
-    function slideCheck(test) {
-        console.log(test.$slides[0]);
+    // function slideCheck(test) {
+    //     console.log(test.$slides[0]);
 
-        let slideItem = test.$slides;
+    //     let slideItem = test.$slides;
 
-        if ( $(slideItem[0]).hasClass("slick-active") ) {
-            // alert("슬라이드1");
-            $(".progress").css("left", "0px");
-            $("body").css("background", "#FFC0CB");
-        } else if ( $(slideItem[1]).hasClass("slick-active") ) {
-            // alert("슬라이드2");
-            $(".progress").css("left", "100px");
-            $("body").css("background", "#FF9BAD");
-        } else if ( $(slideItem[2]).hasClass("slick-active") ) {
-            // alert("슬라이드3");
-            $(".progress").css("left", "200px");
-            $("body").css("background", "#FF7089");
-        } else if ( $(slideItem[3]).hasClass("slick-active") ) {
-            // alert("슬라이드4");
-            $(".progress").css("left", "300px");
-            $("body").css("background", "#FF4C6B");
-        }
-    }
+    //     if ( $(slideItem[0]).hasClass("slick-active") ) {
+    //         // alert("슬라이드1");
+    //         $(".progress").css("left", "0px");
+    //         $("body").css("background", "#FFC0CB");
+    //     } else if ( $(slideItem[1]).hasClass("slick-active") ) {
+    //         // alert("슬라이드2");
+    //         $(".progress").css("left", "100px");
+    //         $("body").css("background", "#FF9BAD");
+    //     } else if ( $(slideItem[2]).hasClass("slick-active") ) {
+    //         // alert("슬라이드3");
+    //         $(".progress").css("left", "200px");
+    //         $("body").css("background", "#FF7089");
+    //     } else if ( $(slideItem[3]).hasClass("slick-active") ) {
+    //         // alert("슬라이드4");
+    //         $(".progress").css("left", "300px");
+    //         $("body").css("background", "#FF4C6B");
+    //     }
+    // }
 
     $(".slider").slick({
         dots: true,
