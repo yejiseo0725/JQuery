2a5602ff09619c951f623a2d6f03d2d0baa1feba
yeji seo <yejiseo0725@gmail.com>
2022-08-08 12:57:25 +0900

    scroll event

---
+++ Scroll_Animation/js/scroll_Ani01.js
@@ -0,0 +1,32 @@
+$(function () {
+  let scrollTop;
+
+  $(window).scroll(function () {
+    scrollTop = $(this).scrollTop();
+    $(".pos").text(scrollTop);
+
+    $(".stars").css({
+      bottom: scrollTop * 0.5 + "%",
+    });
+    $(".stars2").css({
+      bottom: scrollTop * 0.025 + "%",
+    });
+    $(".header").css({
+      top: scrollTop * 0.5 + "px",
+    });
+    $(".moon").css({
+      bottom: -scrollTop * 0.5 + "px",
+    });
+    $(".rock").css({
+      bottom: -scrollTop * 0.5 + "px",
+    });
+    $(".masjid").css({
+      bottom: -scrollTop * 0.1 + "px",
+    });
+
+    $(".ani-text").css({
+      left: 100 + -scrollTop * 0.2 + "%",
+      top: 50 + scrollTop * 0.08 + "%",
+    });
+  });
+});
